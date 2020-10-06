346de1fd9fab3bc759f750880a9a8b95aa840b92
SwiftViktor <panafviktor@gmail.com>
2020-10-06 21:26:44 +0300

    add CLI

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A simple console application written in Rust to display Mandelbrot set.
 
-
+try ./mandelbrot  10000 -- -1.0 1.0 -1.0 1.0 150 50
 
 
 ![Screen 1](https://raw.githubusercontent.com/panviktor/mandelbrot_set/main/screenshot/Screenshot%20_1.png)
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use num::complex::Complex;
+use clap::{App, Arg};
 
 fn calculate_mandelbrot(
     max_iters: usize,
@@ -58,10 +59,77 @@ fn render_mandelbrot(escape_vals: Vec<Vec<usize>>) {
 }
 
 fn main() {
-    let mandelbrot = calculate_mandelbrot(100_000,
-                                                        -1.0, 1.0,
-                                                        -1.0, 1.0,
-                                                        150, 50,
+    let args = App::new("mandelbrot_set")
+        .version("0.2.0")
+        .author("SwiftViktor <panafviktor@gmail.com>")
+        .about(
+            "A simple console application written in Rust to display Mandelbrot set.")
+
+        .arg(Arg::new("max_iters")
+            .about("If a value has not 'escaped' before reaching the maximum number of iterations,\n
+             it is considered to be within the Mandelbrot set")
+            .takes_value(true)
+            .required(true))
+
+        .arg(Arg::new("x_min")
+            .about("These four parameters specify the space we’re searching for to look for members of the set")
+            .takes_value(true)
+            .required(true))
+
+        .arg(Arg::new("x_max")
+            .about("TThese four parameters specify the space we’re searching for to look for members of the set")
+            .takes_value(true)
+            .required(true))
+
+        .arg(Arg::new("y_min")
+            .about("These four parameters specify the space we’re searching for to look for members of the set")
+            .takes_value(true)
+            .required(true))
+
+
+        .arg(Arg::new("y_max")
+            .about("These four parameters specify the space we’re searching for to look for members of the set")
+            .takes_value(true)
+            .required(true))
+
+        .arg(Arg::new("width")
+            //.help("These two parameters represent the width of the output in 'pixels'")
+            .takes_value(true)
+            .required(true))
+
+        .arg(Arg::new("height")
+            .about("These two parameters represent the height of the output in 'pixels'")
+            .takes_value(true)
+            .required(true))
+
+        .get_matches();
+
+
+    let max_iters = args.value_of("max_iters").unwrap();
+    let max_iters_usize: usize = max_iters.parse().unwrap();
+
+    let x_min = args.value_of("x_min").unwrap();
+    let x_min_f64: f64 = x_min.parse().unwrap();
+
+    let x_max = args.value_of("x_max").unwrap();
+    let x_max_f64: f64 = x_max.parse().unwrap();
+
+    let y_min = args.value_of("y_min").unwrap();
+    let y_min_f64: f64 = y_min.parse().unwrap();
+
+    let y_max = args.value_of("y_max").unwrap();
+    let y_max_f64: f64 = y_max.parse().unwrap();
+
+    let width = args.value_of("width").unwrap();
+    let width_usize: usize = width.parse().unwrap();
+
+    let height = args.value_of("height").unwrap();
+    let height_usize: usize = height.parse().unwrap();
+
+    let mandelbrot = calculate_mandelbrot(max_iters_usize,
+                                                        x_min_f64, x_max_f64,
+                                                        y_min_f64, y_max_f64,
+                                                         width_usize, height_usize,
     );
     render_mandelbrot(mandelbrot);
 }
